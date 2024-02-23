55e4697948df0f633d5548611516e63baa8f35c8
Deric <105538170+dericsadrs@users.noreply.github.com>
2024-02-23 22:40:38 +0800

    Start Section 3

--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice_app/components/gradient_container.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-      return  MaterialApp(
-        home: Scaffold(
-          body: GradientContainer.purple()
-        )
-      );
+    return Container(child: const Text("Hello World"),);
   }
 }
