6d87ce40321ea1510b56c645ff0fc0481a369436
Deric <105538170+dericsadrs@users.noreply.github.com>
2024-03-11 11:50:41 +0800

    Section 5 Number 102

---
+++ expense_tracker_app/lib/models/expense.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:uuid/uuid.dart';
+
+
+
+const uuid = Uuid();
+
+enum InputCategory {
+    food,
+    travel,
+    leisure,
+    work
+}
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final InputCategory category;
+}
